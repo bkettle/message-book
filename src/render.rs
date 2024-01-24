0bb6eddbc5003919837d6f041f5191efb9f44aee
Ben Kettle <theyellowtoaster@gmail.com>
2024-01-23 21:57:14 -0900

    switch to rendering messages based on .body()
    This removes the unicode fffc and fffd that were present
    previously. We still have a couple of weird unicode characters
    slipping through, but this took care of almost all of them.

--- src/render.rs
+++ src/render.rs
@@ -1,4 +1,4 @@
-use imessage_database::tables::messages::Message;
+use imessage_database::tables::messages::{Message, BubbleType};
 use regex::Regex;
 
 /// Make necessary replacements so that the text is ready for insertion
@@ -6,9 +6,12 @@ use regex::Regex;
 fn latex_escape(text: String) -> String {
     // TODO: gotta be a more efficient way to do this
     let escaped = text 
+        // first, a bunch of weird characters replaced with ascii
         .replace("’", "'")
         .replace("“", "\"")
         .replace("”", "\"")
+        .replace("…", "...")
+        // now, actual latex escapes
         .replace(r"\", r"\textbackslash\ ")
         .replace("$", r"\$")
         .replace("%", r"\%")
@@ -19,8 +22,10 @@ fn latex_escape(text: String) -> String {
         .replace("#", r"\#")
         .replace(r"{", r"\{")
         .replace(r"}", r"\}")
-        .replace("\u{FFFC}", "[OBJ]");
-        // .replace("\u{FFFD}", "[OBJ]");
+        // this last one is the "variation selector" which I think determines whether an emoji
+        // should be displayed big or inline. The latex font doesn't support it, so we just strip it out.
+        // More info here: https://stackoverflow.com/questions/38100329/what-does-u-ufe0f-in-an-emoji-mean-is-it-the-same-if-i-delete-it
+        .replace("\u{FE0F}", "");
 
     // Now, we wrap emojis in {\emojifont XX}. The latex template has a different font for emojis, and
     // this allows emojis to use that font
@@ -32,17 +37,41 @@ fn latex_escape(text: String) -> String {
 
 }
 
+struct LatexMessage {
+    is_from_me: bool,
+    body_text: Option<String>,
+    attachment_count: i32
+}
+
+impl LatexMessage {
+    fn render(self) -> String {
+        let content = match self.body_text {
+            Some(ref text) => latex_escape(text.to_string()), // probably not ideal to be cloning here
+            None => "< EMPTY MESSAGE >".to_string(),
+        };
+
+        let to_write = match self.is_from_me {
+            // god generating latex code is so annoying with the escapes
+            true => format!("\\leftmsg{{{}}}\n\n", content),
+            false => format!("\\rightmsg{{{}}}\n\n", content),
+        };
+
+        to_write
+    }
+}
+
 pub fn render_message(msg: &Message) -> String {
-    let content = match msg.text {
-        Some(ref text) => latex_escape(text.to_string()), // probably not ideal to be cloning here
-        None => "< EMPTY MESSAGE >".to_string(),
-    };
+    let parts = msg.body();
 
-    let to_write = match msg.is_from_me {
-        // god generating latex code is so annoying with the escapes
-        true => format!("\\leftmsg{{{}}}\n\n", content),
-        false => format!("\\rightmsg{{{}}}\n\n", content),
-    };
+    let mut latex_msg = LatexMessage { is_from_me: msg.is_from_me, body_text: None, attachment_count: 0 };
 
-    to_write
+    for part in parts {
+        match part {
+            BubbleType::Text(text) => { latex_msg.body_text = Some(text.to_owned())},
+            BubbleType::Attachment => { latex_msg.attachment_count += 1 }
+            _ => ()
+        }
+    }
+
+    latex_msg.render()
 }
